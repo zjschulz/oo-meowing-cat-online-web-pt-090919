6690553e39526151d94d9e3e010ac69972e7b563
Zachary Schulz <zjschulz@gmail.com>
2019-09-27 00:37:37 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,2 +1,17 @@
 class Cat 
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
